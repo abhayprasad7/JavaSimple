b5f78cc53a27df6e1a544068ea033496b5622238
trainee <trainee>
2018-07-16 16:13:07 +0530

    ver 1

---
+++ dev2/src/dev2/Hello.java
@@ -0,0 +1,11 @@
+package dev2;
+
+public class Hello {
+	
+	    public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	    }
+
+	
+}
